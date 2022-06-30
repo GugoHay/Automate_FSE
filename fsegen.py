901b379a8f5937da0e64a422d5a73416b6fda724
Gugo <67103293+Hamzouz@users.noreply.github.com>
2022-06-30 11:01:21 +0200

    Add files via upload

---
+++ fsegen.py
@@ -0,0 +1,27 @@
+txt = """[PRESTATION]
+Quantite=
+Coefficient=
+Code_CCAM=
+Code_compl_CCAM=
+Modificateurs_CCAM=
+Code_suppl_CCAM=
+Code_affine=
+Qualificatif_depense=
+Domicile=
+D_jf=
+Nuit=
+Urgence=
+Code=
+Montant_honoraires= 
+"""
+
+def filecreator():
+    x,y = 1,15
+    while x < y :
+        with open("FSE" + str(x).zfill(8) + ".hif","w") as f:
+            f.write(txt)
+            f.close()
+            x = x + 1
+
+
+filecreator()
