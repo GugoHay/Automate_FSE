cfdd73beee6444b1bf837931626b226b5f718314
Gugo <67103293+GugoHay@users.noreply.github.com>
2022-10-06 10:52:12 +0200

    Update FseGenV2.py
    Ajout de plusieurs section tel que : 
    [Beneficiaire]
    [Assure]
    [Medico_administratif]
    [IRIS]
    [AMC]
    [Prescription]
    [Assurance]
    [Remboursement]

--- FseGenV2.py
+++ FseGenV2.py
@@ -23,15 +23,15 @@ listdomic = []
 listdjf = []
 listurgen = []
 listnuit = []
-# CCAM
-listcodeccam = []
 
 #Fonction d'ajout de prix au format str selon le nombre d'index dans la liste "myList".
 def ajout_hono(laListe):
     for p in mylist:
-        laListe.append(str(random.randint(473,999)))
-        laListe.sort()
-        laListe.reverse()
+        #permet d'attribuer un prix entre deux valeurs
+        #laListe.append(str(random.randint(473,999)))
+
+        # "?" permet à pyx d'assigner le prix de base secu automatiquement
+        laListe.append("?")
 
 #Fonction d'ajout de caractère selon le nombre d'index dans la liste "myList".
 def ajout_neant(laListe):
@@ -54,15 +54,10 @@ def ajout_compl(laListe):
     for p in mylist:
         laListe.append("10")
 
-def ajout_codeccam(laListe):
-    for p in mylist:
-        laListe.append("CCAM")
-
 #Fonction de creation flux actes
 def filecreator():
-    #Variable global (A changer car ce n'est pas une bonne façon de faire. j'ai trouver que sa pour que les valeurs soit pris dans le code partout.)
+    
     global mylist
-    global listcodeccam
     global listdepen
     global listmodif
     global listsuppl
@@ -80,7 +75,7 @@ def filecreator():
     while nbMin < nbMax :
         z = random.randint(1,20)
         mylist = random.sample(ccam, z)
-        #Dans cette partie on incrémente grâce au fonction ajout_ les caractère pour chaque index de mylist (à modifier, car on peut simplifier)
+        #Dans cette partie on incrémente grâce au fonction ajout_ les caractères pour chaque index de mylist (à modifier, car on peut simplifier)
         ajout_neant(listdepen)
         ajout_trait(listmodif)
         ajout_trait(listsuppl)
@@ -91,17 +86,76 @@ def filecreator():
         ajout_uino(listurgen)
         ajout_uino(listnuit)
         ajout_compl(listcompl)
-        ajout_codeccam(listcodeccam)
         ajout_hono(hono)
         #Creation de fichier .hif avec le suivie des numéro de fse
             #with open(f"FSE{str(x).zfill(9)}.hif","w") as f:
         #Creation de fichier .par pour les flux actes_.par
-        with open(f"Actes_FSE{str(nbMin).zfill(9)}.par","w") as f:
-        #Ici la variable txt, prend en compte les listes et les transformes en string avec une séparation pour chaque valeur dans les listes
-            txt = f"""[Prestation]
+        with open(f"CVActes_{str(nbMin).zfill(9)}.par","w") as f:    
+            
+            beneficiaire_txt = f"""
+[Beneficiaire]
+Nom=AIMETE
+Prenom=VINCENT
+Date_de_naissance=17/11/1988
+Rang_gemellaire=1
+Qualite=Assuré
+
+[Assure]
+Numero=1881134170052
+Cle=26
+
+[Medico_administratif]
+Code_regime=01
+Caisse_gestionnaire=349
+Centre_gestionnaire=9881
+
+"""
+            iris = f"""
+[IRIS]
+Nature_piece_justificative=0
+Date=05/04/2022
+
+"""
+            amc = f"""
+[AMC]
+Gestion=S
+Attestation=O
+Organisme=1100000000
+Date1=01/01/2000
+Date2=01/01/2099
+Traitement=33
+Formule=052$0
+
+"""
+            prescription = f"""
+[Prescription]
+Date=14/11/2020
+Numero=99100010
+Cle=0
+Code_specialite=01
+
+"""
+            assu = f"""
+[Assurance]
+Nature=10
+Accident=N
+Alsace_Moselle=N
+
+"""
+            rembou = f"""
+[Remboursement]
+Tiers_payant_partie_obligatoire=O
+AMC=N
+Tiers_payant_partie_complémentaire=O
+Medecin_SNCF=N
+
+"""
+#Ici la variable txt, prend en compte les listes et les transformes en string avec une séparation pour chaque valeur dans les listes
+            txt = f"""
+[PRESTATION]
 Quantite={str(("+").join(listquant))}
-Code_CCAM={str(("+").join(mylist))}
 Coefficient={str(("+").join(listquant))}
+Code_CCAM={str(("+").join(mylist))}
 Code_compl_CCAM={str(("+").join(listcompl))}
 Modificateurs_CCAM={str(("+").join(listmodif))}
 Code_suppl_CCAM={str(("+").join(listsuppl))}
@@ -111,13 +165,11 @@ Domicile={str(("+").join(listdomic))}
 D_jf={str(("+").join(listdjf))}
 Nuit={str(("+").join(listnuit))}
 Urgence={str(("+").join(listurgen))}
-Code={str(("+").join(listcodeccam))}
+Code=
 Montant_honoraires={str(("+").join(hono))}
 """
-            f.write(txt)
+            f.write(beneficiaire_txt + iris + amc + prescription + assu + rembou + txt)
             f.close()
-        #On incrémente de 1 la valeur de x pour éviter la boucle infini
-            nbMin = nbMin + 1
         #une fois notre fichier écrit, on clear les listes pour repartir à zéro car on reste dans la boucle tant que x > y.
             mylist.clear()
             listdepen.clear()
@@ -130,8 +182,9 @@ Montant_honoraires={str(("+").join(hono))}
             listquant.clear()
             listsuppl.clear()
             listurgen.clear()
-            listcodeccam.clear()
             hono.clear()
+        #On incrémente de 1 la valeur de x pour éviter la boucle infini
+            nbMin = nbMin + 1
 
 
 #Lancement de ma fonction de creation flux actes
