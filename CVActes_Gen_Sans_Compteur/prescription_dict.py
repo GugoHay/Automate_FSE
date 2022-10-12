50c315541dab1b55da2f3a5959b149227e1e4005
Gugo <67103293+GugoHay@users.noreply.github.com>
2022-10-12 15:13:29 +0200

    Add files via upload

---
+++ CVActes_Gen_Sans_Compteur/prescription_dict.py
@@ -0,0 +1,11 @@
+#Dictionnaire de prescripteurs
+#Peut être amélioré avec mon algo_luhn et des dates au hasard
+
+prescription_dict = {'presc_1':f"""
+[Prescription]
+Date=01/05/2022
+Numero=99100010
+Cle=0
+Code_specialite=01
+"""
+}
---
+++ CVActes_Gen_Sans_Compteur/remboursement_dict.py
@@ -0,0 +1,10 @@
+#Dictionnaire des rembrousements
+
+remboursement_dict = {'remb_1' : f"""
+[Remboursement]
+Tiers_payant_partie_obligatoire=O
+AMC=O
+Tiers_payant_partie_complementaire=O
+Medecin_SNCF=N
+"""
+}
