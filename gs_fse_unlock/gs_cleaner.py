857ad00d81f166df352850e0ea6b9db13ad657c3
Gugo <67103293+GugoHay@users.noreply.github.com>
2022-10-21 17:46:34 +0200

    Update gs_cleaner.py

--- gs_fse_unlock/gs_cleaner.py
+++ gs_fse_unlock/gs_cleaner.py
@@ -1,10 +1,11 @@
 #Autor : Gurguen Sahakian
 #Description : Dévérouillage automatique des FSE en erreur.
 
-#Fonction de cleaner qui prend en compte "ErreurFSE=A" si présent il n'éffectue aucune action, si non il néttoie le fichier et copie son contenue en mémoire dans un fichier à part.
-#Il déplace le .fs_ dans le dossier temp et le renomme en .fse
-#seul les fichiers .fs_ sont déplacer, les .hif dans le dossier temp sont des fichiers comportant le même nom mais l'intérieur est clean.
-#Il suffit de déplacer les fichiers temp dans le dossier courant pour que le deverouillage soit pris en compte.
+# Fonction de cleaner qui prend en compte "ErreurFSE=A" si présent il n'éffectue aucune action, si non il néttoie le fichier et copie son contenue en mémoire dans un fichier à part.
+# Il déplace le .fs_ dans le dossier temp et le renomme en .fse
+# Seul les fichiers .fs_ sont déplacer, les .hif dans le dossier temp sont des fichiers comportant le même nom mais l'intérieur est clean.
+# Il suffit de déplacer les fichiers temp dans le dossier courant pour que le deverouillage soit pris en compte.
+# Les encodages sont pris en compte à la lecture et ecriture des nouveaux fichiers.
 
 import glob
 import shutil
